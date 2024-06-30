e0104be4db972239307923a9dbe1f238559ff48f
Timo <ttimoww@gmail.com>
2024-06-30 12:50:57 +0200

    changed indexed-based loop to for-of-loop

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -7,52 +7,52 @@ export class GildedTros {
     }
 
     public updateQuality(): void {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Good Wine' && this.items[i].name != 'Backstage passes for Re:Factor'
-                && this.items[i].name != 'Backstage passes for HAXX') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'B-DAWG Keychain') {
-                        this.items[i].quality = this.items[i].quality - 1;
+        for (const item of this.items) {
+            if (item.name != 'Good Wine' && item.name != 'Backstage passes for Re:Factor'
+                && item.name != 'Backstage passes for HAXX') {
+                if (item.quality > 0) {
+                    if (item.name != 'B-DAWG Keychain') {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (this.items[i].name == 'Backstage passes for Re:Factor' || this.items[i].name == 'Backstage passes for HAXX') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
+                    if (item.name == 'Backstage passes for Re:Factor' || item.name == 'Backstage passes for HAXX') {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (this.items[i].name != 'B-DAWG Keychain') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+            if (item.name != 'B-DAWG Keychain') {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Good Wine') {
-                    if (this.items[i].name != 'Backstage passes for Re:Factor' && this.items[i].name != 'Backstage passes for HAXX') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'B-DAWG Keychain') {
-                                this.items[i].quality = this.items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (item.name != 'Good Wine') {
+                    if (item.name != 'Backstage passes for Re:Factor' && item.name != 'Backstage passes for HAXX') {
+                        if (item.quality > 0) {
+                            if (item.name != 'B-DAWG Keychain') {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
