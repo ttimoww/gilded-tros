b9ad234ed5374c8b3fff4b064c4ec67068360274
Timo <ttimoww@gmail.com>
2024-06-30 12:48:10 +0200

    unit tests and bugfix

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -1,4 +1,4 @@
-import {Item} from './item';
+import { Item } from './item';
 
 export class GildedTros {
 
@@ -19,7 +19,7 @@ export class GildedTros {
                 if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1;
 
-                    if (this.items[i].name == 'Backstage passes for Re:Factor') {
+                    if (this.items[i].name == 'Backstage passes for Re:Factor' || this.items[i].name == 'Backstage passes for HAXX') {
                         if (this.items[i].sellIn < 11) {
                             if (this.items[i].quality < 50) {
                                 this.items[i].quality = this.items[i].quality + 1;
@@ -41,7 +41,7 @@ export class GildedTros {
 
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Good Wine') {
-                    if (this.items[i].name != 'Backstage passes for Re:Factor' || this.items[i].name != 'Backstage passes for HAXX') {
+                    if (this.items[i].name != 'Backstage passes for Re:Factor' && this.items[i].name != 'Backstage passes for HAXX') {
                         if (this.items[i].quality > 0) {
                             if (this.items[i].name != 'B-DAWG Keychain') {
                                 this.items[i].quality = this.items[i].quality - 1;
--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -1,9 +1,80 @@
-import {Item} from '../src/item';
-import {GildedTros} from '../src/gilded-tros';
+import { Item } from '../src/item';
+import { GildedTros } from '../src/gilded-tros';
 
 describe('GildedTrosTest', () => {
-    const items: Item[] = [new Item('foo', 0, 0)];
-    const app: GildedTros = new GildedTros(items);
-    app.updateQuality();
-    expect(app.items[0].name).toEqual('fixme');
+    it('should update the quality', () => {
+        const items: Item[] = [new Item('foo', 0, 0)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].name).toEqual('foo');
+    });
+
+    it('should decrease the quality of a normal item', () => {
+        const items: Item[] = [new Item('foo', 1, 1)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(0);
+        expect(app.items[0].sellIn).toEqual(0);
+    });
+
+    it('should decrease the quality of a normal item twice as fast once the sell date passed', () => {
+        const items: Item[] = [new Item('foo', -1, 2)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(0);
+        expect(app.items[0].sellIn).toEqual(-2);
+    });
+
+    it('should never have a negative quality', () => {
+        const items: Item[] = [new Item('foo', 1, 0)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(0);
+    });
+
+    it('should increase the quality of "Good Wine" but never above 50', () => {
+        const items: Item[] = [new Item('Good Wine', 2, 0), new Item('Good Wine', 2, 50)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(1);
+        expect(app.items[1].quality).toEqual(50);
+    })
+
+    it('should never decrease the quality of "B-DAWG Keychain"', () => {
+        const items: Item[] = [new Item('B-DAWG Keychain', 0, 80)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(80);
+    });
+
+    it('should drop the quality of expired conference tickets to 0', () => {
+        const items: Item[] = [new Item('Backstage passes for Re:Factor', 0, 20), new Item('Backstage passes for HAXX', 0, 20)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(0);
+        expect(app.items[1].quality).toEqual(0);
+    });
+
+    it('should increase the quality of conference tickets by 2 when there are 10 days or less', () => {
+        const items: Item[] = [new Item('Backstage passes for Re:Factor', 10, 20), new Item('Backstage passes for HAXX', 10, 20)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(22);
+        expect(app.items[1].quality).toEqual(22);
+    });
+
+    it('should increase the quality of conference tickets by 3 when there are 5 days or less', () => {
+        const items: Item[] = [new Item('Backstage passes for Re:Factor', 5, 20), new Item('Backstage passes for HAXX', 5, 20)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(23);
+        expect(app.items[1].quality).toEqual(23);
+    });
+
+    it('should keep the quality of legendary items at 80', () => {
+        const items: Item[] = [new Item('B-DAWG Keychain', 0, 80)];
+        const app: GildedTros = new GildedTros(items);
+        app.updateQuality();
+        expect(app.items[0].quality).toEqual(80);
+    });
 });
